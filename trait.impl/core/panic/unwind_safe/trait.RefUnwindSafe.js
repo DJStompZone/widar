ff64002498e255cc85548b4a90844150e7de5e11
DJStompZone <DJStompZone@users.noreply.github.com>
2024-05-17 23:00:42 +0000

    deploy: c0b8dcdc58846f04ec02af659e7adf582eab1f9d

--- trait.impl/core/panic/unwind_safe/trait.RefUnwindSafe.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"widar":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"widar/struct.NumSignalBars.html\" title=\"struct widar::NumSignalBars\">NumSignalBars</a>",1,["widar::NumSignalBars"]]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
